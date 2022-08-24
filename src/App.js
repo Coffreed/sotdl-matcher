32dd503a82e274e617d179034b41e2079976adb7
Edward <edward.freitas@hiq.fi>
2022-08-24 13:04:16 +0300

    Implement recommend card - Change App

--- src/App.js
+++ src/App.js
@@ -10,13 +10,39 @@ function App() {
   const [inputedDifficulty, setInputedDifficulty] = useState([]);
 
   useEffect(()=>{
-    if (selectedType.length !== 0 || inputedDifficulty.length !== 0){
+    if ((selectedType.length !== 0 || inputedDifficulty.length !== 0)){
       const filteredList = bestiary.filter((best) => {
-        const isDifficulty = inputedDifficulty.length === 0 || parseInt(inputedDifficulty) === best.difficulty;
+        const isDifficulty = inputedDifficulty.length === 0 || (parseInt(inputedDifficulty) === best.difficulty || parseInt(inputedDifficulty) > best.difficulty);
         const isSelectedType = selectedType.length === 0 || selectedType.includes(best.creatureType);
         return isSelectedType && isDifficulty;
       })
+      const sumFiltered = filteredList.reduce((a, b) => a = a + b.difficulty, 0);
+      const parseInput = parseInt(inputedDifficulty);
+      
+      filteredList.sort(() => 0.5 - Math.random());
+      console.log('sumFiltered IS THIS', sumFiltered);
       setCardList(filteredList);
+      if (sumFiltered !== parseInput){
+        const filteredList2 = bestiary.filter((best) => {
+          const isDifficulty = parseInt(inputedDifficulty) === best.difficulty;
+          const isSelectedType = selectedType.length === 0 || selectedType.includes(best.creatureType);
+          return isSelectedType && isDifficulty;
+        })
+        filteredList2.sort(() => 0.5 - Math.random());
+        filteredList2.sort((a, b) => b.difficulty - a.difficulty);
+        const returnedBeast = filteredList2.slice(0, 1);
+        setCardList(returnedBeast);
+        const sumFiltered2 = filteredList2.reduce((a, b) => a = a + b.difficulty, 0);
+        const numberByLenght = filteredList2.length;
+        const sumFiltered3 = sumFiltered2;
+        console.log("ed go home you are tired", numberByLenght);
+        const filteredTotal = sumFiltered2 / numberByLenght;
+        console.log('return numberByLenght ed you dumb bro', numberByLenght);
+        console.log('return my bs', filteredTotal);
+        console.log('sumFilte333333333333333333333tered3', sumFiltered3);
+        
+        console.log('return filteredList2', filteredList2);
+      }
     } else {
       setCardList(bestiary);
     }
