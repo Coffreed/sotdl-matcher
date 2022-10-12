da999114ceab8c3b5d6e737d65a613916e96a5e5
Edward <edward.freitas@hiq.fi>
2022-10-12 10:18:17 +0300

    Implement matcher algorithm - Changes App

--- src/App.js
+++ src/App.js
@@ -13,101 +13,83 @@ function App() {
   const [searchByName, setSearch] = useState([]);
 
   useEffect(()=>{
-    // generates a random number 0 or 1 for filter
-    const totalGenerator = Math.floor(Math.random() * 2);
-    // primarily detects input
-    if ((selectedType.length !== 0 || inputedDifficulty.length !== 0 || searchByName.length !== 0)){
-      // stores input value
+    if (inputedDifficulty.length !== 0){
       const parseInput = parseInt(inputedDifficulty);
-      // filteres & stores difficulty 1 monsters
-      const getOnes = bestiary.filter(best => {
-        return best.difficulty === 1;
-      });
-      // filteres & stores difficulty 10 monsters
-      const getTens = bestiary.filter(best => {
-        return best.difficulty === 10;
-      });
-      // filteres & stores difficulty 25 monsters
-      const getTwentyFives = bestiary.filter(best => {
-        return best.difficulty === 25;
-      });
-      // filteres & stores difficulty 50 monsters
-      const getFiftys = bestiary.filter(best => {
-        return best.difficulty === 50;
-      });
-      // filteres & stores difficulty 100 monsters
-      const getHundreds = bestiary.filter(best => {
-        return best.difficulty === 100;
-      });
-      // filteres & stores difficulty 250 monsters
-      const getTwoHundredFiftys = bestiary.filter(best => {
-        return best.difficulty === 250;
-      });
-      // filteres & stores difficulty 500 monsters
-      const getFiveHundreds = bestiary.filter(best => {
-        return best.difficulty === 500;
-      });
-
-      // totals & stores the values of all difficulties
-      const getOnesTotal = getOnes.reduce((a, b) => a = a + b.difficulty, 0);
-      const getTensTotal = getTens.reduce((a, b) => a = a + b.difficulty, 0);
-      const getTwentyFivesTotal = getTwentyFives.reduce((a, b) => a = a + b.difficulty, 0);
-      const getFiftysTotal = getFiftys.reduce((a, b) => a = a + b.difficulty, 0);
-      const getHundredsTotal = getHundreds.reduce((a, b) => a = a + b.difficulty, 0);
-      const getTwoHundredFiftysTotal = getTwoHundredFiftys.reduce((a, b) => a = a + b.difficulty, 0);
-      const getFiveHundredTotal = getFiveHundreds.reduce((a, b) => a = a + b.difficulty, 0);
-
-      console.log(getOnesTotal, "1");
-      console.log(getTensTotal, "10");
-      console.log(getTwentyFivesTotal, "25");
-      console.log(getFiftysTotal, "50");
-      console.log(getHundredsTotal, "100");
-      console.log(getTwoHundredFiftysTotal, "250");
-      console.log(getFiveHundredTotal, "500");
-      console.log('THE INPUT', parseInput);
-
-      // detects parse input value and tries to match it with totals
-      if (parseInput === getOnesTotal || (parseInput === getTensTotal && totalGenerator === 1) || (parseInput === getTwentyFivesTotal && totalGenerator === 1) ||
-      selectedType.length !== 0 || searchByName.length !== 0 || parseInput === getFiftysTotal || parseInput === getHundredsTotal || 
-      (parseInput === getTwoHundredFiftysTotal && totalGenerator === 1) || parseInput === getFiveHundredTotal){
-        // filter looks to display result that was in criteria
+      // Run combinationSum with bestiary and parseInput
+      const matcher = combinationSum(bestiary, parseInput);
+      
+      function combinationSum(arr, sum) {
+        let ans = new Array();
+        let temp = new Array();
+        
+        // Sort list from lowest value to the largest
+        const sortedArr = arr.sort((a, b) => a.difficulty - b.difficulty);
+        
+        // Used to prevent using same value more than once
+        let unusedArr = [...sortedArr];
+      
+        findNumbers(ans, sortedArr, sum, 0, temp, unusedArr);
+        console.log(ans, "output");
+        return ans;
+      }
+      function findNumbers(ans, arr, sum, index, temp, unusedArr) {
+        // Match is completed
+        if (sum === 0) {
+          // pushing deep copy of list to ans
+          ans.push([...temp]);
+          // reset unusedArr
+          unusedArr = [...arr];
+          // Break the recursion
+          return;
+        }
+        for (let i = index; i < unusedArr.length; i++) {
+          if (sum - unusedArr[i].difficulty >= 0) {
+            // pushing element which can contribute to sum
+            temp.push(unusedArr[i].id);
+            // remove used value from unused array for the rest of this scenario
+            const newUnused = [...unusedArr];
+            newUnused.splice(i, 1);
+            findNumbers(ans, arr, sum - unusedArr[i].difficulty, i, temp, newUnused);
+      
+            // removing element from list (backtracking)
+            temp.splice(temp.indexOf(unusedArr[i]), 1);
+          }
+        }
+      }
+      if (!matcher.length) {
         const filteredList = bestiary.filter((best) => {
           // checks & stores if difficulty was input and checks if the value was less than inputed value
-          const isDifficulty = inputedDifficulty.length === 0 || parseInput > best.difficulty;
+          const isDifficulty = inputedDifficulty.length === 0 || parseInput === best.difficulty;
           // checks & stores if selected type was input and matches with the creature type
           const isSelectedType = selectedType.length === 0 || selectedType.includes(best.creatureType);
           // checks & stores if search by name was input and matches with the name of the creature
           const isNamed = searchByName.length === 0 || searchByName.includes(best.name);
           return isSelectedType && isDifficulty && isNamed;
         })
-        // sorts result in random
-        filteredList.sort(() => 0.5 - Math.random());
-        // sorts result by highest difficulty accessible
-        filteredList.sort((a, b) => b.difficulty - a.difficulty);
-        console.log(filteredList, 'CHECK HOW it is filtering')
-        // totals and stores total amount of all difficulties in data
-        const totalDifficultyFilteredList = filteredList.reduce((a, b) => a = a + b.difficulty, 0);
-        console.log('totalDifficultyFilteredList', totalDifficultyFilteredList);
-        // checks if total difficulty in filtered list is less than inputted value
-        if (totalDifficultyFilteredList > parseInput) {
-          const getCards = getMultipleCards({parseInput, getOnes, getTens, getTwentyFives, getFiftys, getHundreds, getTwoHundredFiftys, getFiveHundreds});
-          setCardList(getCards);
-        } else {
-          setCardList(filteredList);
-        }
+        setCardList(filteredList);
       } else {
-        const filteredList2 = bestiary.filter((best) => {
-          const isDifficulty = parseInput === best.difficulty;
-          const isSelectedType = selectedType.length === 0 || selectedType.includes(best.creatureType);
-          const isNamed = searchByName.length === 0 || searchByName.includes(best.name);
-          return isSelectedType && isDifficulty && isNamed;
-        })
-        filteredList2.sort(() => 0.5 - Math.random());
-        filteredList2.sort((a, b) => b.difficulty - a.difficulty);
-        const returnedBeast = filteredList2.slice(0, 1);
-        setCardList(returnedBeast);
+        // Get random combination
+        const randomIndex = getRandomInt(0, matcher.length - 1);
+        const randomCombination = matcher[randomIndex];
+      
+        // find matching monster by id's
+        const monsters = randomCombination.map((i) =>
+        bestiary.find((m) => m.id === i)
+        );
+        console.log(monsters);
+        setCardList(monsters);
       }
-    } else {
+    } else if (selectedType.length !== 0 || searchByName.length !== 0) {
+      const filteredList2 = bestiary.filter((best) => {
+        // checks & stores if selected type was input and matches with the creature type
+        const isSelectedType = selectedType.length === 0 || selectedType.includes(best.creatureType);
+        // checks & stores if search by name was input and matches with the name of the creature
+        const isNamed = searchByName.length === 0 || searchByName.includes(best.name);
+        return isSelectedType && isNamed;
+      })
+      setCardList(filteredList2);
+    }
+    else {
       setCardList(bestiary);
     }
   }, [selectedType, inputedDifficulty, searchByName])
@@ -122,6 +104,12 @@ function App() {
 
   function onSearchChange(name){
     setSearch(name);
+  }
+
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
   }
 
   return (
